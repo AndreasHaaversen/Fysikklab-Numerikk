870baf3043df9a5ac0cb0761157fbf11ec5ea5bc
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-03-30 12:50:37 +0200

    Fix plot.py

--- plot.py
+++ plot.py
@@ -4,8 +4,8 @@ import calculate
 
 plt.rcParams.update({'font.size': 20})
 
-(xy, data) = calculate.calculate(selected="ball")
-time_data = calculate.calculate_with_time(selected="ball")
+(xy, data) = calculate.calculate(selected="hollow_disk")
+time_data = calculate.calculate_with_time(selected="hollow_disk")
 
 x = xy["x"]
 y = xy["y"]
@@ -29,11 +29,18 @@ for key, value in data.items():
     plt.grid()
     plt.show()
 
-for key, value in time_data.items():
-    plt.figure(''+key, figsize=graph_size)
-    plt.title(value[1])
-    plt.plot(x, value[0])
-    plt.xlabel('$t$ [s]', fontsize=20)
-    plt.ylabel('$' + key + '$(t)' + " " + value[2], fontsize=20)
-    plt.grid()
-    plt.show()
+plt.figure('Fart som funksjon av tid', figsize=graph_size)
+plt.title('Fart som funksjon av tid')
+plt.plot(time_data, data["v"][0])
+plt.xlabel('$t$ [s]', fontsize=20)
+plt.ylabel('$v$(t)' + " " + value[2], fontsize=20)
+plt.grid()
+plt.show()
+
+plt.figure('Posisjon som funksjon av tid', figsize=graph_size)
+plt.title('Posisjon som funksjon av tid')
+plt.plot(time_data, x)
+plt.xlabel('$t$ [s]', fontsize=20)
+plt.ylabel('$v$(t)' + " " + value[2], fontsize=20)
+plt.grid()
+plt.show()
